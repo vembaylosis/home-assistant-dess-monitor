db664d0578b9abbef02dd854e2c79ca6eb28e9d2
Antoxa1081 <antoxa1081@gmail.com>
2025-04-02 17:19:10 +0300

    Add diagnostics

---
+++ custom_components/dess_monitor/diagnostics.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceEntry
+
+
+async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry) -> dict:
+    """Return diagnostics for a config entry."""
+    # Example data, replace with real data gathering
+
+    return {
+        "config_entry": {
+            "title": entry.title,
+            "data": async_redact_data(entry.data, ['username', 'password_hash']),
+            "options": dict(entry.options),
+        },
+    }
+
+
+async def async_get_device_diagnostics(
+        hass: HomeAssistant, entry: ConfigEntry, device: DeviceEntry,
+) -> dict[str, Any]:
+    """Return diagnostics for a device entry."""
+    return {
+        "device": {
+            'pn': device.model,
+            'devcode': device.hw_version,
+            'data': entry.runtime_data.coordinator.data[device.model]
+        }
+    }
